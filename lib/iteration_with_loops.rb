d96f7a7e704848d8c036757ee09cc34b8f5a7a9f
Joshua Singleton <jsingle345@gmail.com>
2020-05-01 22:29:22 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -10,7 +10,8 @@ def join_nested_strings(src)
       total = total + src[row_index][element_index]
       element_index += 1 
     end
-    row_index +=binding.pry
+    row_index += 
+    binding.pry
   end
   total
   
